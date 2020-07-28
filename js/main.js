98d255e3fa9a6dce13a5f9e25fc7d49a24ff1004
Vladymyr <vladymyr.a13@gmail.com>
2020-07-28 07:41:00 +0300

    Завершена верстка блока slider-item__info

--- js/main.js
+++ js/main.js
@@ -2,5 +2,9 @@ $(function () {
   $(".header__slider").slick({
     infinity: true,
     fade: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="" />',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="" />',
   });
 });
