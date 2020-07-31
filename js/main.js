84c8bd5ceabeb53584610c89145678f70128591c
Vladymyr <vladymyr.a13@gmail.com>
2020-07-31 07:38:48 +0300

    Связал два слайдера хедера.

--- js/main.js
+++ js/main.js
@@ -6,5 +6,12 @@ $(function () {
       '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="" />',
     nextArrow:
       '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="" />',
+    asNavFor: ".slider-dots",
+  });
+
+  $(".slider-dots").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: ".header__slider",
   });
 });
