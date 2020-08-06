a8445b43fe3c21a6c14f64c741854e2bf6e87a4e
Vladymyr <vladymyr.a13@gmail.com>
2020-08-06 07:30:29 +0300

    к секции surf подключил слик слайдер

--- js/main.js
+++ js/main.js
@@ -14,4 +14,13 @@ $(function () {
     slidesToScroll: 4,
     asNavFor: ".header__slider",
   });
+
+  $(".surf-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="" />',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="" />',
+  });
 });
