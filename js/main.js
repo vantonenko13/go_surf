c713dfc439bb72122b5d67cbb025143a7da82d03
Vladymyr <vladymyr.a13@gmail.com>
2020-08-10 00:53:14 +0300

    Сверстан блок surf со слайдером и картой

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ $(function () {
       '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="" />',
     nextArrow:
       '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="" />',
-    asNavFor: ".slider-dots",
+    asNavFor: ".slider-dotshead",
   });
 
-  $(".slider-dots").slick({
+  $(".slider-dotshead").slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: ".header__slider",
@@ -22,5 +22,14 @@ $(function () {
       '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="" />',
     nextArrow:
       '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="" />',
+    asNavFor: ".slider-map",
+  });
+
+  $(".slider-map").slick({
+    slidesToShow: 8,
+    slideToScroll: 1,
+    arrows: false,
+    asNavFor: ".surf-slider",
+    focusOnSelect: true,
   });
 });
