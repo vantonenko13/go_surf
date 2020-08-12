43a682b8fb6036fc1bf1da844925ca61ebe9b82f
Vladymyr <vladymyr.a13@gmail.com>
2020-08-12 23:59:27 +0300

    Сверстана секция travel

--- js/main.js
+++ js/main.js
@@ -32,4 +32,13 @@ $(function () {
     asNavFor: ".surf-slider",
     focusOnSelect: true,
   });
+
+  $(".travel__slider").slick({
+    infinity: true,
+    fade: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="" />',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="" />',
+  });
 });
